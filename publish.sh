f39cfa2b08eec2634c25c9b3d07f307f4b84858a
benedikt-mayer <benedikt_mayer@outlook.de>
2019-03-31 19:16:51 +0200

    added shell script for publishing

---
+++ publish.sh
@@ -0,0 +1,27 @@
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout hakyll
+
+# Build new files
+stack exec CleanMagic-hakyll rebuild
+
+# Get previous files
+git fetch --all
+git checkout -b master --track origin/master
+
+# Overwrite existing files with new files
+cp -a _site/. .
+
+# Commit
+git add -A
+git commit -m "Publish."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout hakyll
+git branch -D master
+git stash pop
