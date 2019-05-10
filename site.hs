d48196310adc1b2597a07abfd32c09f17533bcd5
benedikt-mayer <benedikt_mayer@outlook.de>
2019-05-10 16:27:46 +0200

    configured stack.yaml so that the GHC version of HIE and the project match up

--- site.hs
+++ site.hs
@@ -63,83 +63,47 @@ main = hakyllWith config $ do
   match "templates/*" $ compile templateCompiler
 
 {- match "posts/*" $ do
-
   route $ setExtension "html"
-
   compile
-
     $   pandocCompiler
-
     >>= loadAndApplyTemplate "templates/post.html"    postCtx
-
     >>= loadAndApplyTemplate "templates/default.html" postCtx
-
     >>= relativizeUrls -}
-
 {-   create ["index.html"] $ do
-
     route idRoute
-
     compile $ do
-
       projects <- recentFirst =<< loadAll "projects/*"
-
       let projectsCtx =
-
             listField "projects" siteCtx (return projects)
-
               <> constField "title" "Hi!"
-
               <> siteCtx
 
-
-
       makeItem ""
-
         >>= loadAndApplyTemplate "templates/gallery.html" projectsCtx
-
         >>= loadAndApplyTemplate "templates/default.html" projectsCtx
-
         >>= relativizeUrls -}
 
-
 {-   match "index.html" $ do
-
       route idRoute
-
       compile $ do
-
           posts <- recentFirst =<< loadAll "posts/*"
-
           let indexCtx =
-
                   listField "posts" postCtx (return posts) <>
-
                   -- constField "title" "Home"                <>
-
                   siteCtx
-
           getResourceBody
-
               >>= applyAsTemplate indexCtx
-
               >>= loadAndApplyTemplate "templates/default.html" indexCtx
-
               >>= relativizeUrls -}
-
-
-
 
 --------------------------------------------------------------------------------
 
 
 -- post context with date field
-
 postCtx :: Context String
 postCtx = dateField "date" "%B %e, %Y" <> siteCtx
 
 -- normal site context - used for all pages as of now.
-
 siteCtx :: Context String
 siteCtx =
   constField "baseurl" "http://localhost:35730"
@@ -153,6 +117,5 @@ siteCtx =
     <> defaultContext
 
 -- configuration for display
-
 config :: Configuration
 config = defaultConfiguration { previewHost = "0.0.0.0", previewPort = 35729 }
