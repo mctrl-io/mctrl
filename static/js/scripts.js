41c3ac6b3ba2bddf068dae1d3988c27fc3cc6bfa
benedikt-mayer <benedikt_mayer@outlook.de>
2019-05-11 18:38:29 +0200

    hides home button on menu click for mobile. also removed commented google analytics javascript for now.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,37 +1,10 @@
 var mainNav = document.getElementById("js-menu")
 var navBarToggle = document.getElementById("js-navbar-toggle")
-var projectNav = document.getElementById("js-projects-menu")
-var projectNavBarToggle = document.getElementById("js-navbar-projects-toggle")
+var homeNav = document.getElementById("js-home")
 
 navBarToggle.addEventListener("click", function () {
     mainNav.classList.toggle("active")
+    homeNav.children[0].classList.toggle("hidden")
     navBarToggle.children[0].classList.toggle("fa-bars")
     navBarToggle.children[0].classList.toggle("fa-times")
 })
-
-projectNavBarToggle.addEventListener("click", function () {
-    projectNav.classList.toggle("active")
-})
-
-
-//<![CDATA[
-// $(window).load(function() { // makes sure the whole site is loaded
-//   $('#status').fadeOut(); // will first fade out the loading animation
-//   $('#preloader').delay(150).fadeOut('fast'); // will fade out the white DIV that covers the website.
-//   $('body').delay(100).css({'overflow':'visible'});
-// })
-//]]>
-
-// !function ($) {
-//   $(function(){
-//     window.prettyPrint && prettyPrint()
-//   })
-// }(window.jQuery)
-
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// ga('create', 'UA-XXXXXXXX-X', 'auto');
-// ga('send', 'pageview');
