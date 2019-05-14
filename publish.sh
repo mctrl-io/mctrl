c8e2886dca994f76d3d1c499a351615d85e8e7a3
benedikt-mayer <benedikt_mayer@outlook.de>
2019-05-14 15:21:57 +0200

    added .gitmodules to filter in publish

--- publish.sh
+++ publish.sh
@@ -23,6 +23,7 @@ if [ $yesno == "y" ]; then
         --filter='P _cache/' \
         --filter='P .git/' \
         --filter='P .gitignore' \
+        --filter='P .gitmodules' \
         --filter='P .stack-work' \
         --delete-excluded \
         _site/ .
